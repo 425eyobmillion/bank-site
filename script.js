5dc546075ea870a7e832ebd0a9e9976896a9c227
eyob million <eyobmillion425@gmail.com>
2023-08-01 16:41:29 +0300

    update

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,9 +19,13 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(function(btn){
+  btn.addEventListener('click',openModal);
+});
 
+/*for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+*/
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
@@ -29,3 +34,42 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+
+/*const header=document.querySelector('.header');
+const mssg=document.createElement('div');
+mssg.classList.add('cookie-message');
+
+mssg.innerHTML='we use cookies for improved functionality and anlaytics.<button class="btn btn--close-cookie">Got it!</button>';
+header.append(mssg);
+
+const cok=document.querySelector('.btn--close-cookie');
+cok.addEventListener('click',function(){
+  mssg.remove();
+});
+
+//style
+mssg.style.backgroundColor='#37383d';
+mssg.style.width='128%';
+mssg.style.height=Number.parseFloat(getComputedStyle(mssg).
+height,10)+30+'px';
+
+//document.documentElement.style.setProperty('--color-primary','orangered');
+*/
+
+const btnsc=document.querySelector('.btn--scroll-to');
+const section1=document.querySelector('#section--1');
+
+btnsc.addEventListener('click',function(){
+  const s1cords=section1.getBoundingClientRect();
+
+  //scrolling
+ /* window.scrollTo({
+    left:s1cords.left+window.pageXOffset
+    ,top:s1cords.top+window.pageYOffset,
+    behavior:'smooth'
+  });*/
+
+  section1.scrollIntoView({behavior:'smooth'});
+})
