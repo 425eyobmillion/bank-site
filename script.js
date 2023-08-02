c610d401cfc2327d6d1c6251a049a4602f1ccf6e
eyob million <eyobmillion425@gmail.com>
2023-08-02 15:21:14 +0300

    new

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -19,13 +25,8 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-btnsOpenModal.forEach(function(btn){
-  btn.addEventListener('click',openModal);
-});
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
-/*for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
-*/
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
@@ -35,88 +36,80 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+///////////////////////////////////////
+// Button scrolling
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
 
+  console.log(e.target.getBoundingClientRect());
 
-/*const header=document.querySelector('.header');
-const mssg=document.createElement('div');
-mssg.classList.add('cookie-message');
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
 
-mssg.innerHTML='we use cookies for improved functionality and anlaytics.<button class="btn btn--close-cookie">Got it!</button>';
-header.append(mssg);
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
 
-const cok=document.querySelector('.btn--close-cookie');
-cok.addEventListener('click',function(){
-  mssg.remove();
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
 
-//style
-mssg.style.backgroundColor='#37383d';
-mssg.style.width='128%';
-mssg.style.height=Number.parseFloat(getComputedStyle(mssg).
-height,10)+30+'px';
+///////////////////////////////////////
+// Page navigation
 
-//document.documentElement.style.setProperty('--color-primary','orangered');
-*/
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
 
-const btnsc=document.querySelector('.btn--scroll-to');
-const section1=document.querySelector('#section--1');
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
 
-btnsc.addEventListener('click',function(){
-  const s1cords=section1.getBoundingClientRect();
-
-  //scrolling
- /* window.scrollTo({
-    left:s1cords.left+window.pageXOffset
-    ,top:s1cords.top+window.pageYOffset,
-    behavior:'smooth'
-  });*/
-
-  section1.scrollIntoView({behavior:'smooth'});
-})
-
-const h1=document.querySelector('h1');
-
-/*const alerth1=h1.addEventListener('mouseenter',function(e){
-  alert('addeventlistner:you are reading');
-})
-
-setTimeout(()=>h1.removeEventListener('mouseenter',alerth1),3000);*/
-
-const alerth1=function(e){
-  alert('addeventlistner:great you are reading the heading');
-};
-
-h1.addEventListener('mouseenter',alerth1);
-
-setTimeout(()=>h1.removeEventListener('mouseenter',alerth1),3000);
-
-//rgb(255,255,255);
-const randomint=(min,max) =>
-  Math.floor(Math.random()*(max-min+1)+min);
-const randomcolor=()=>
-  `rgb(${randomint(0,255)},${randomint(0,255)},${randomint(0,255)})`;
-
-document.querySelector('.nav_link').addEventListener('click',function(e){
-  this.style.backgroundColor =randomcolor();
-})
-
-//section scrolling
-/*document.querySelectorAll('.nav__links').forEach(function(el){
-  el.addEventListener('click',function(e) {
-    e.preventDefault();
-     const id=this.getAttribute('href');
-     document.querySelector(id).scrollIntoView({behavior:'smooth'});
-     console.log(id);
-  });
-});*/
-
-document.querySelector('.nav__link').addEventListener('click',function(e){
+document.querySelector('.nav__links').addEventListener('click', function (e) {
   e.preventDefault();
-   
-  if(e.target.classList.contains('.nav__links')){
-    const id=e.target.getAttribute('href');
-    document.querySelector(id).scrollIntoView({behavior:'smooth'})
-  }
 
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
 });
 
+///////////////////////////////////////
+// Tabbed component
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
